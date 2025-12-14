b3b9bacd49ca06f2674454d23c985b9ded4e274b
contactusforbusiness07-cloud <contactusfor.business07@gmail.com>
2025-12-14 17:46:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -57,11 +57,11 @@ app.get('/api/user/:id', async (req, res) => {
 
 // --- 3. SERVE FRONTEND (Ye Naya Part Hai) ---
 // Server ko batao ki frontend files kahan hain
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Koi bhi aur link khule to React App dikhao
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
